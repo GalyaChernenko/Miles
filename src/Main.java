4c31f7bd76c1e93b123ad097826a98b5256faf58
Chernenko <galka8@inbox.ru>
2022-12-19 21:52:37 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 3177; //стоимость билета;
+        int percent = 5; //процент начисления;
+        int miles = price * percent / 100; //количество начисленных миль;
+        System.out.println(miles);
+
+
+    }
+}
